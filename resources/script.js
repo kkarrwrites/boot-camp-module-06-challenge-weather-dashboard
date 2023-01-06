58f3e727dfe216fe65f7b2f83d1de1be0e2f0b7c
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-01-06 15:19:23 -0500

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -3,58 +3,89 @@
 const searchFormEl = document.querySelector("#search-form");
 const searchInputEl = document.querySelector("#search-input");
 
+const cityContainerEl = document.querySelector(".city");
+const cityNameEl = document.querySelector("#city-name");
+const cityDateEl = document.querySelector("#city-date");
+const cityEmojiEl = document.querySelector("#city-emoji");
+
+const currentTemp = document.querySelector("#temp-current");
+const currentWind = document.querySelector("#wind-current");
+const currentHumidity = document.querySelector("#humidity-current");
+
+const forecastContainerEl = document.querySelector("#forecast");
+
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  let citySearched = searchInputEl.value.trim();
+  const citySearched = searchInputEl.value.trim();
 
   if (citySearched) {
-    console.log(citySearched);
     const apiKey = "f4340e47ea2c893cd45e033c791f26e0";
     const apiUrl =
       "http://api.openweathermap.org/data/2.5/forecast?q=" +
       citySearched +
       "&units=imperial&appid=" +
       apiKey;
-    console.log(apiUrl);
+    // console.log(apiUrl);
+
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+        });
+      }
+    });
+
     searchInputEl.value = "";
   } else {
     window.alert("You must input a city name to get a weather report.");
   }
 }
 
-searchFormEl.addEventListener("submit", handleSearchFormSubmit);
+function displayWeather(data) {
+  // When I view current weather conditions for that city, then I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed.
 
-/* const searchFormEl = document.querySelector("#search-form");
-const searchInputVal = document.querySelector("#search-input");
+  // Display city weather container
+  cityContainerEl.classList.remove("hidden");
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
+  // Display city name
+  cityNameEl.textContent = data.city.name;
 
-  let searchInputValTrim = searchInputVal.value.trim();
+  // Display the date
+  const date = data.list[0].dt_txt;
+  cityDateEl.textContent = dayjs(date).format("M/D/YYYY");
 
-  if (searchInputValTrim) {
-    getCityWeather(searchInputValTrim);
+  // Display an icon representation of weather conditions
+  const emoji = data.list[0].weather[0].main;
+
+  if (emoji === "Atmosphere") {
+    cityEmojiEl.textContent === "🌫️";
+  } else if (emoji === "Clear") {
+    cityEmojiEl.textContent === "☀️";
+  } else if (emoji === "Clouds") {
+    cityEmojiEl.textContent = "☁️";
+  } else if (emoji === "Drizzle") {
+    cityEmojiEl.textContent = "🌧️";
+  } else if (emoji === "Rain") {
+    cityEmojiEl.textContent = "🌧️";
+  } else if (emoji === "Snow") {
+    cityEmojiEl.textContent = "🌨️";
+  } else if (emoji === "Thunderstorm") {
+    cityEmojiEl.textContent = "⛈️";
   } else {
-    window.alert("You must search for a city by the city's name.");
-    return;
+    cityEmojiEl.textContent = "";
   }
-}
 
-function getCityWeather() {
-  let apiUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    searchInputVal +
-    "&limit=1&appid=f4340e47ea2c893cd45e033c791f26e0";
+  // Display the temperature
+  currentTemp.textContent = data.list[0].main.temp + " °F";
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      console.log(response);
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
+  // Display the the wind speed
+  currentWind.textContent = data.list[0].wind.speed + " MPH";
+
+  // Display the humidity
+  currentHumidity.textContent = data.list[0].main.humidity + " %";
+
+  // When I view future weather conditions for that city, then I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 }
 
 searchFormEl.addEventListener("submit", handleSearchFormSubmit);
- */
